8f4b15a2fead75c53acf7c69082c9b73656167e8
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2023-11-15 17:17:52 -0300

    feat: customize main screen

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:roll_dice_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('Hello World!'),
+      ),
+    );
+  }
+}
